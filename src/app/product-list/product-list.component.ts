bb266e8da2d66e40483ffb989861b5b4acb16fc8
ruvindalakmal <ruvindalakmal18@gmail.com>
2024-12-07 21:12:33 +0530

    Event Binding

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -7,15 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
+  // name = 'John Doe';
+  addToCart : number = 0;
   product = {
     name  : 'IPhone 12Pro',
     price  : 1199,
     color  : 'Gold',
     discount  : 10,
-    inStock : 0,
+    inStock : 10,
     pImage : 'assets/iphone_12pro.jpg'
   }
   getDiscountedPrice(){
     return this.product.price - (this.product.price * this.product.discount / 100);
+  }
+  // onNameChange(event : any){
+  //    this.name = event.target.value;
+  // }
+  decrementCartValue(){
+    if(this.addToCart > 0){
+      this.addToCart--;
+    }
+  }
+  incrementCartValue(){
+    if(this.addToCart < this.product.inStock){
+      this.addToCart++;
+    }
   }
 }
