5a18b55380522514bb8d0d39dd59b8a80747dd50
ruvindalakmal <ruvindalakmal18@gmail.com>
2024-12-09 22:40:57 +0530

    ngFor Directive

---
+++ src/app/header/main-menu/main-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {NgForOf} from '@angular/common';
+
+@Component({
+  selector: 'main-menu',
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css'
+})
+export class MainMenuComponent {
+  mainMenuItems : string[] = ['Home' , 'Products' , 'Sale' , 'New Arrival' , 'Contacts'];
+}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,36 +1,16 @@
 import { Component } from '@angular/core';
+import {SearchComponent} from './search/search.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'product-list',
-  imports: [],
+  imports: [
+    SearchComponent,
+    NgForOf
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  // name = 'John Doe';
-  addToCart : number = 0;
-  product = {
-    name  : 'IPhone 12Pro',
-    price  : 1199,
-    color  : 'Gold',
-    discount  : 10,
-    inStock : 10,
-    pImage : 'assets/iphone_12pro.jpg'
-  }
-  getDiscountedPrice(){
-    return this.product.price - (this.product.price * this.product.discount / 100);
-  }
-  // onNameChange(event : any){
-  //    this.name = event.target.value;
-  // }
-  decrementCartValue(){
-    if(this.addToCart > 0){
-      this.addToCart--;
-    }
-  }
-  incrementCartValue(){
-    if(this.addToCart < this.product.inStock){
-      this.addToCart++;
-    }
-  }
+  listOfStrings : string[] = ['Mark' , 'Steve' , 'Merry' , 'John' , 'Sarah'];
 }
